695f1c49543643755357d853e24180ba5ffc4bbc
Felipe Siqueira <felipefvs@gmail.com>
2023-12-23 11:13:32 -0300

    inicial

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require('dotenv').config()
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world! Port: " + PORT);
+});
+
+app.listen(PORT);
